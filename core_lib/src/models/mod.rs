c47a7bfa1292a487b885af0ea0aab74cd4967a6d
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-22 21:16:50 +0300

    Added multiple modules for the models

--- core_lib/src/models/mod.rs
+++ core_lib/src/models/mod.rs
@@ -1,3 +1,9 @@
 pub mod request;
+pub mod items;
+pub mod auth;
+pub mod files;
 
-pub use request::{JsonPayload, FormPayload};
+pub use request::{JsonPayload, FormPayload, ApiResponse};
+pub use items::*;
+pub use auth::*;
+pub use files::*;
