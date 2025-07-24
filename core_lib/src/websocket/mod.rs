6fde3994668ba50e1bd59dcd7f2bb1bdd8405cc8
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-24 18:03:02 +0300

    Added a tests module

--- core_lib/src/websocket/mod.rs
+++ core_lib/src/websocket/mod.rs
@@ -2,6 +2,9 @@ pub mod handler;
 pub mod manager;
 pub mod messages;
 
+#[cfg(test)]
+mod tests;
+
 pub use handler::websocket_handler;
 pub use manager::{WebSocketManager, WebSocketConnection};
 pub use messages::{WebSocketMessage, WebSocketEvent};
